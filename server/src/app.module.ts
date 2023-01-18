0de4be9b445b2d119b8bb6f8025eaf65e84745a8
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-01-18 23:54:13 +0700

    add user to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { UserController } from './controller/user.controller';
+import { User, UserSchema } from './model/user.schema';
+import { UserService } from './service/user.service';
 import { JwtModule } from '@nestjs/jwt';
 import { secret } from './utils/constants';
 import { join } from 'path/posix';
+import { isAuthenticated } from './app.middleware';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/Pet_Cafe'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     JwtModule.register({
       secret,
       signOptions: { expiresIn: '2h' },
@@ -18,7 +23,16 @@ import { join } from 'path/posix';
       rootPath: join(__dirname, '..', 'public'),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule {
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer
+  //     .apply(isAuthenticated)
+  //     .exclude(
+  //       { path: 'api/v1/video/:id', method: RequestMethod.GET }
+  //     )
+  //     .forRoutes(...);
+  // }
+}

f8754e17423f3f49351a40b5efde0f9c5e750e67
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-07-11 10:12:50 +0700

    add tab navigator

--- client-mobile/App.tsx
+++ client-mobile/App.tsx
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './screens/HomeScreen';
+import DrinkMenuScreen from './screens/DrinkMenuScreen';
+import PetMenuScreen from './screens/PetMenuScreen';
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = 'home';
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Drinks') {
+              iconName = focused ? 'cafe' : 'cafe-outline';
+            } else if (route.name === 'Pets') {
+              iconName = focused ? 'paw' : 'paw-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'violet',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name='Drinks' component={DrinkMenuScreen} />
+        <Tab.Screen name='Pets' component={PetMenuScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ client-mobile/screens/DrinkMenuScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Text, View } from "react-native"
+export default function DrinkMenuScreen(){
+    return (
+        <View>
+            <Text>Drink Menu Page!</Text>
+        </View>
+    )
+}

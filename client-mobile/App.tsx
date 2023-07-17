c835db301805520611734eb1b6ca1f2ee5862ad5
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-07-17 11:34:22 +0700

    add DrinksStack & change routes name

--- client-mobile/App.tsx
+++ client-mobile/App.tsx
@@ -2,23 +2,32 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import HomeScreen from "./screens/HomeScreen";
-import ProductList from "./screens/DrinkMenuScreen";
-import PetMenuScreen from "./screens/PetMenuScreen";
+import DrinkMenu from "./screens/Menu/DrinkMenuScreen";
+import PetMenuScreen from "./screens/Menu/PetMenuScreen";
 import SignIn from "./screens/Auth/SignInScreen";
 import SignUp from "./screens/Auth/SignUpScreen";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { navigationRef } from "./components/RootNavigation";
+import ProductList from "./screens/Menu/MenuDetailScreen";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 function HomeStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="HomeMain" component={HomeScreen} />
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+}
+function DrinksStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="DrinksType" component={DrinkMenu} />
+      <Stack.Screen name="ProductList" component={ProductList} />
     </Stack.Navigator>
   );
 }
@@ -46,8 +55,8 @@ export default function App() {
             tabBarInactiveTintColor: "gray",
           })}
         >
-          <Tab.Screen name="HomeStack" component={HomeStack} />
-          <Tab.Screen name="Drinks" component={ProductList} />
+          <Tab.Screen name="Home" component={HomeStack} />
+          <Tab.Screen name="Drinks" component={DrinksStack} />
           <Tab.Screen name="Pets" component={PetMenuScreen} />
         </Tab.Navigator>
       </NavigationContainer>
